71df86f2d1b467a395a9d25201336e73b8ee8f36
Isa <isarodriguez3001.gr@gmail.com>
2024-12-02 16:00:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+/*let nombre = `Gabriela`; //variables que el valor puede variar. (Comilla abierta catalán)
+const edad = 22; //variables cuyo valor no va a cambiar, es constante
+console.log (`Hola mi nombre es ${nombre}`);
+console.log (`Tengo ${edad} años`);
+let año = 2002;
+console.log (`Naci en ${año}`);
+
+let resultado = 10 + 5;
+console.log (`El resultado es ${resultado}`);
+
+let esMayor = 10 > 5 ; // mayor (>), menor (<)
+console.log (`Quien es mayor ${esMayor}`);
+
+
+let esVerdadero = true && false;
+
+
+
+if (edad >= 18){
+    console.log (`Eres mayor de edad.`)
+} else{
+    console.log (`Eres menor de edad`)
+}
+*/
+
+
+//Ejercicios:
+
+
+// 1. Crea una variable nombre con tu nombre y úsala para imprimir un mensaje.
+let nombre = `Isabel`
+console.log(`Mi nombre es ${nombre}`)
+
+// 2. Declara y suma dos números, muestra el resultado en consola.
+let resultado = 10 + 5;
+console.log (`El resultado es: ${resultado}`);
+
+// 3. Usa operadores lógicos para evaluar si 10 > 5 y 5 < 2.
+let esMayor = 10 > 5 ; // mayor (>), menor (<)
+console.log (`Es mayor ${esMayor}`);
+
+let esMenor = 5 < 2 ; // mayor (>), menor (<)
+console.log (`Es menor ${esMenor}`);
+
+let esVerdadero = true && false;
+
+
+// 4. Declara variables con let y const y nota las diferencias al intentar cambiarlas.
+let animal = `Gato`;
+const numero = 174;
+animal = `Perro`
+console.log (`El animal es: ${animal}`)
+
+// 5. Escribe un comentario que explique qué hace tu código.
+// 6. Declara una variable de tipo boolean y úsala en una operación lógica.
+// 7. Imprime el tipo de dato de una variable usando typeof.
+// 8. Declara un array y muestra uno de sus elementos en la consola.
+// 9. Usa una operación con % para encontrar el resto de 17 ÷ 3.
+// 10. Declara una variable sin valor e imprime su valor inicial.
+
+
+
+
+
+
+
